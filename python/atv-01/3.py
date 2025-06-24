968112a7d739c2394679125f9e0dfbda3a115d6a
DevSaLLein <devzac.andrade@gmail.com>
2025-06-24 19:47:09 -0300

    feat: adicionada a primeira atividade prática

---
+++ python/atv-01/3.py
@@ -0,0 +1,15 @@
+#3- Calculadora de Volume
+#* Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+
+#* Comprimento: 12 cm
+#* Largura: 14 cm
+#* Altura: 20 cm
+#O programa deve calcular o volume e exibir o resultado em cm³.
+
+altura = 20;
+largura = 14;
+comprimento = 12;
+
+volume = altura * largura * comprimento;
+
+print("O volume da caixa é: ", volume, "cm³");
